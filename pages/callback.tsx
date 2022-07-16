f21945fb6b355cc4af8f5508cf06157c397f0120
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-17 05:30:26 +0900

    Update callback.tsx

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const _: NextPage = () => {
+  const { handleRedirectCallback } = useAuth0();
   const router = useRouter();
 
   useEffect(() => {
-    router.replace((router.query.backTo as string | undefined) ?? "/");
-  }, [router]);
+    handleRedirectCallback().then(() => {
+      router.replace((router.query.backTo as string | undefined) ?? "/");
+    });
+  }, [handleRedirectCallback, router]);
 
   return <></>;
 };
