e60461d36610876bc91e6388cdd4f653f821ff55
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-17 03:01:37 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -80,7 +80,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
       redirectUri={`${process.env.NEXT_PUBLIC_BASE_URI!}/callback`}
     >
       <QueryClientProvider client={queryClient}>
