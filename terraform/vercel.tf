dfb4016a22146a1ef21457c976c3d61d7faae5ad
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-17 02:42:43 +0900

    Update vercel.tf

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -3,14 +3,32 @@ provider "vercel" {
 }
 
 resource "vercel_project" "default" {
-  name = terraform.workspace
+  name      = terraform.workspace
+  framework = "nextjs"
+
 
   git_repository = {
     type = "github"
     repo = var.github_repo
   }
 
-  framework = "nextjs"
+  environment = [
+    {
+      key    = "NEXT_PUBLIC_AUTH0_DOMAIN"
+      value  = var.auth0_domain
+      target = ["production"]
+    },
+    {
+      key    = "NEXT_PUBLIC_AUTH0_CLIENT_ID"
+      value  = auth0_client.a0sandbox_frontend.client_id
+      target = ["production"]
+    },
+    {
+      key    = "NEXT_PUBLIC_BASE_URI"
+      value  = "https://${terraform.workspace}.vercel.app"
+      target = ["production"]
+    },
+  ]
 }
 
 output "project_url" {
