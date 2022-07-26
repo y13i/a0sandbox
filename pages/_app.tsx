3e63999d32b1fab44bde9d18a0f6334eff4f18b1
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-27 05:37:44 +0900

    AppNavigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import type { AppProps } from "next/app";
-import Link from "next/link";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import "@fontsource/roboto/300.css";
@@ -8,75 +6,16 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import BaseAppBar, { AppBarProps } from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import BaseDrawer from "@mui/material/Drawer";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Divider from "@mui/material/Divider";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
-import LoginIcon from "@mui/icons-material/Login";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-import { Menu } from "../components/Menu";
+import { AppNavigation } from "../components/AppNavigation";
 
 const queryClient = new QueryClient();
 
-const drawerWidth = 240;
-
-const AppBar = styled(BaseAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps & { open: boolean }>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(BaseDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  "& .MuiDrawer-paper": {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: "border-box",
-    ...(!open && {
-      overflowX: "hidden",
-      transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up("sm")]: {
-        width: theme.spacing(9),
-      },
-    }),
-  },
-}));
-
 function App({ Component, pageProps }: AppProps) {
-  const [open, setOpen] = useState(false);
-  const toggleDrawer = () => setOpen(!open);
-
   return (
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
@@ -85,57 +24,12 @@ function App({ Component, pageProps }: AppProps) {
     >
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={createTheme()}>
+          <CssBaseline />
           <Box sx={{ display: "flex" }}>
-            <CssBaseline />
-            <AppBar position="absolute" open={open}>
-              <Toolbar
-                sx={{
-                  pr: "24px", // keep right padding when drawer closed
-                  justifyContent: "space-between",
-                }}
-              >
-                <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  onClick={toggleDrawer}
-                  sx={{
-                    mr: 2,
-                    ...(open && { visibility: "hidden" }),
-                  }}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Button color="inherit" startIcon={<LoginIcon />}>
-                  TODO: Login
-                </Button>
-              </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-              <Toolbar
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  px: [1],
-                }}
-              >
-                <IconButton>
-                  <Link href="/" passHref>
-                    <HomeIcon />
-                  </Link>
-                </IconButton>
-                <IconButton onClick={toggleDrawer}>
-                  <ChevronLeftIcon />
-                </IconButton>
-              </Toolbar>
-              <Divider />
-              <Menu disableTooltip={open} />
-            </Drawer>
+            <AppNavigation />
             <Box
               component="main"
-              sx={{ px: 3, py: 10, flexGrow: 1, overflow: "scroll" }}
+              sx={{ px: 3, py: 10, flexGrow: 1, overflow: "auto" }}
             >
               <Component {...pageProps} />
             </Box>
