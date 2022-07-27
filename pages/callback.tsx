7670996b484a62f5aab78ea444add7dafa7f2eb7
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-27 18:46:29 +0900

    Update callback.tsx

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -1,15 +1,30 @@
 import { useEffect } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import Typography from "@mui/material/Typography";
+
+import { WithHead } from "../components/WithHead";
 
 const _: NextPage = () => {
   const router = useRouter();
+  const { isReady, returnTo, error, error_description } = router.query;
 
   useEffect(() => {
-    router.replace((router.query.returnTo as string | undefined) ?? "/");
-  }, [router]);
+    if (isReady && !error) {
+      router.replace((returnTo as string | undefined) ?? "/");
+    }
+  }, [router, isReady, returnTo, error]);
 
-  return <></>;
+  return (
+    <WithHead title="Callback" description="Auth0 authentication callback.">
+      <Typography variant="h3" gutterBottom>
+        Error: {error}
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        {error_description}
+      </Typography>
+    </WithHead>
+  );
 };
 
 export default _;
