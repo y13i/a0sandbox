15795738bd182d85cf7724baa2c499790b560a94
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-07-27 17:05:29 +0900

    Update logout.tsx

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,15 +1,19 @@
 import { useEffect } from "react";
 import type { NextPage } from "next";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from "next/router";
 
 import { WithHead } from "../components/WithHead";
 
 const _: NextPage = () => {
   const { logout } = useAuth0();
+  const router = useRouter();
+
+  const { returnTo } = router.query;
 
   useEffect(() => {
-    logout();
-  }, [logout]);
+    logout({ returnTo: `${process.env.NEXT_PUBLIC_BASE_URI!}/${returnTo}` });
+  }, [logout, returnTo]);
 
   return (
     <WithHead title="Logout" description="Logout.">
