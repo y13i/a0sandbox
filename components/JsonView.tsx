9636493a90f87d944caac5b85003128007b1509a
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-08-02 19:15:37 +0900

    Update JsonView.tsx

--- components/JsonView.tsx
+++ components/JsonView.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import dynamic from "next/dynamic";
 import { ComponentProps } from "react";
 
@@ -6,5 +7,7 @@ const _ = dynamic(import("react-json-view"), {
 });
 
 export const JsonView: typeof _ = (props: ComponentProps<typeof _>) => (
-  <_ name={false} quotesOnKeys={false} {...props} />
+  <Box sx={{ mt: "5px" }}>
+    <_ name={false} quotesOnKeys={false} {...props} />
+  </Box>
 );
