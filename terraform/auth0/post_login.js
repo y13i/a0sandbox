929ef2e6d06f23aafe4499263095301b0de6fbea
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-08-02 22:52:06 +0900

    Update post_login.js

--- terraform/auth0/post_login.js
+++ terraform/auth0/post_login.js
@@ -1,3 +1,24 @@
+const { createLogger, transports, format } = require("winston");
+const { ManagementClient } = require("auth0");
+
+const logger = createLogger({
+  transports: [new transports.Console()],
+  level: "debug",
+  format: format.combine(
+    format.timestamp(),
+    format.metadata({
+      fillExcept: ["timestamp", "level", "message"],
+    }),
+    format.json()
+  ),
+});
+
+logger.debug("logger initialized");
+
 exports.onExecutePostLogin = async (event, api) => {
-  api.user.setUserMetadata("extensibilityDebug", { ...event.user.user_metadata.extensibilityDebug, "action-post-login": { event } })
+  const managementClient = new ManagementClient({
+    domain: event.secrets.domain,
+    clientId: event.secrets.clientId,
+    clientSecret: event.secrets.clientSecret,
+  });
 };
