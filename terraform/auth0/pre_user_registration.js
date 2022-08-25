babd169693bd139d4f7a4c297e2ec2be594f6f60
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-08-25 23:55:08 +0900

    Update pre_user_registration.js

--- terraform/auth0/pre_user_registration.js
+++ terraform/auth0/pre_user_registration.js
@@ -1,3 +1,8 @@
 exports.onExecutePreUserRegistration = async (event, api) => {
-  api.user.setAppMetadata("extensibilityDebug", { "action_pre-user-registration": { event } });
+  api.user.setAppMetadata("extensibilityDebug", {
+    "action_pre-user-registration": {
+      ...event,
+      secrets: "REDUCTED",
+    }
+  });
 };
